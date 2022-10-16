37c85a5d598ebc85d1e05f10e338935f5d40483e
mixture <dragonlorefnstattrack@gmail.com>
2022-10-16 21:58:22 +0600

    added nuxt-start without git repo

---
+++ nuxt-start/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Title :title="title" />
+    <nuxt-link to="/about">Go to About</nuxt-link>
+    <h2>{{ $store.state.title }}</h2>
+    <button @click="increment">Increment Me!</button>
+    <h3>{{ $store.state.counter }}</h3>
+  </div>
+</template>
+
+<script>
+import Title from "~/components/Title.vue";
+export default {
+  name: "IndexPage",
+  components: { Title },
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    },
+  },
+  data() {
+    return {
+      title: "Homepage!",
+    };
+  },
+};
+</script>

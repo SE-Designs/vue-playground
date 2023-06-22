60019d41caf023c500894bf7a27fc6c2f6df0b17
mixture <dragonlorefnstattrack@gmail.com>
2023-06-22 21:17:02 +0600

    pinia - lesson 7: composition api

--- pinia/src/stores/movieStore.js
+++ pinia/src/stores/movieStore.js
@@ -1,55 +1,96 @@
+import { computed, ref } from "vue";
+
 import { defineStore } from "pinia";
 
 // Options API store:
-export const useMovieStore = defineStore("movieStore", {
-	// state - variables:
-	state: () => ({
-		movies: [
-			{
-				id: 1,
-				original_title: "Spider-Man",
-				overview:
-					"After being bitten by a genetically altered spider at Oscorp, nerdy but endearing high school student Peter Parker is endowed with amazing powers to become the superhero known as Spider-Man.",
-				poster_path: "/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg",
-				release_date: "2002-05-01",
-				isWatched: false,
-			},
-			{
-				id: 2,
-				original_title: "The Batman",
-				overview:
-					"In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.",
-				poster_path: "/b0PlSFdDwbyK0cf5RxwDpaOJQvQ.jpg",
-				release_date: "2022-03-01",
-				isWatched: true,
-			},
-		],
-		activeTab: 2,
-	}),
-	// getters - for filtering and showing:
-	getters: {
-		watchedMovies() {
-			return this.movies.filter((el) => el.isWatched);
-		},
-		totalWatchedMovies() {
-			return this.movies.filter((el) => el.isWatched).length;
-		},
-		totalMovies() {
-			return this.movies.length;
-		},
-	},
-	// to change the state
-	// synchronous actions:
-	actions: {
-		setActiveTab(id) {
-			this.activeTab = id;
-		},
-		toggleWatch(id) {
-			const idx = this.movies.findIndex((el) => el.id === id);
-			this.movies[idx].isWatched = !this.movies[idx].isWatched;
-		},
-		deleteMovie(id) {
-			this.movies = this.movies.filter((el) => el.id !== id);
-		},
-	},
+// export const useMovieStore = defineStore("movieStore", {
+// 	// state - variables:
+// 	state: () => ({
+// 		movies: [
+// 			{
+// 				id: 1,
+// 				original_title: "Spider-Man",
+// 				overview:
+// 					"After being bitten by a genetically altered spider at Oscorp, nerdy but endearing high school student Peter Parker is endowed with amazing powers to become the superhero known as Spider-Man.",
+// 				poster_path: "/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg",
+// 				release_date: "2002-05-01",
+// 				isWatched: false,
+// 			},
+// 			{
+// 				id: 2,
+// 				original_title: "The Batman",
+// 				overview:
+// 					"In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.",
+// 				poster_path: "/b0PlSFdDwbyK0cf5RxwDpaOJQvQ.jpg",
+// 				release_date: "2022-03-01",
+// 				isWatched: true,
+// 			},
+// 		],
+// 		activeTab: 2,
+// 	}),
+// 	// getters - for filtering and showing:
+// 	getters: {
+// 		watchedMovies() {
+// 			return this.movies.filter((el) => el.isWatched);
+// 		},
+// 		totalWatchedMovies() {
+// 			return this.movies.filter((el) => el.isWatched).length;
+// 		},
+// 		totalMovies() {
+// 			return this.movies.length;
+// 		},
+// 	},
+// 	// to change the state
+// 	// synchronous actions:
+// 	actions: {
+// 		setActiveTab(id) {
+// 			this.activeTab = id;
+// 		},
+// 		toggleWatch(id) {
+// 			const idx = this.movies.findIndex((el) => el.id === id);
+// 			this.movies[idx].isWatched = !this.movies[idx].isWatched;
+// 		},
+// 		deleteMovie(id) {
+// 			this.movies = this.movies.filter((el) => el.id !== id);
+// 		},
+// 	},
+// });
+
+export const useMovieStore = defineStore("moviestore", () => {
+	const movies = ref([]);
+	const activeTab = ref(1);
+
+	const watchedMovies = computed(() =>
+		movies.value.filter((el) => el.isWatched)
+	);
+
+	const totalWatchedMovies = computed(
+		() => movies.value.filter((el) => el.isWatched).length
+	);
+
+	const totalMovies = computed(() => movies.value.length);
+
+	const setActiveTab = (id) => {
+		activeTab.value = id;
+	};
+
+	const toggleWatch = (id) => {
+		const idx = movies.value.findIndex((el) => el.id === id);
+		movies.value[idx].isWatched = !movies.value[idx].isWatched;
+	};
+
+	const deleteMovie = (id) => {
+		movies.value = movies.value.filter((el) => el.id !== id);
+	};
+
+	return {
+		movies,
+		activeTab,
+		watchedMovies,
+		totalWatchedMovies,
+		totalMovies,
+		setActiveTab,
+		toggleWatch,
+		deleteMovie,
+	};
 });
--- pinia/src/stores/searchStore.js
+++ pinia/src/stores/searchStore.js
@@ -1,29 +1,59 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 import { useMovieStore } from "./movieStore";
 
 const url =
 	"https:api.themoviedb.org/3/search/movie?api_key=02708c4929ad93aa5e68f8ee7bfa4445&query=";
 
-export const useSearchStore = defineStore("searchStore", {
-	state: () => ({
-		loader: false,
-		movies: [],
-	}),
-	// assynchronous actions:
-	actions: {
-		async getMovies(searchQuery) {
-			this.loader = true;
-			const res = await fetch(url + searchQuery);
-			const data = await res.json(res);
+// export const useSearchStore = defineStore("searchStore", {
+// 	state: () => ({
+// 		loader: false,
+// 		movies: [],
+// 	}),
+// 	actions: {
+// 		// assynchronous actions:
+// 		async getMovies(searchQuery) {
+// 			this.loader = true;
+// 			const res = await fetch(url + searchQuery);
+// 			const data = await res.json();
 
-			this.movies = data.results;
-			this.loader = false;
-		},
-		addFavourite(movie) {
-			const movieStore = useMovieStore();
+// 			this.movies = data.results;
+// 			this.loader = false;
+// 		},
+// 		// using different store inside this store
+// 		addFavourite(movie) {
+// 			const movieStore = useMovieStore();
 
-			movieStore.movies.push({ ...movie, isWatched: false });
-			movieStore.activeTab = 1;
-		},
-	},
+// 			movieStore.movies.push({ ...movie, isWatched: false });
+// 			movieStore.activeTab = 1;
+// 		},
+// 	},
+// });
+
+export const useSearchStore = defineStore("searchstore", () => {
+	const loader = ref("");
+	const movies = ref("");
+
+	const getMovies = async (searchQuery) => {
+		loader.value = true;
+		const res = await fetch(url + searchQuery);
+		const data = await res.json();
+
+		movies.value = data.results;
+		loader.value = false;
+	};
+
+	const addFavourite = (movie) => {
+		const movieStore = useMovieStore();
+
+		movieStore.movies.push({ ...movie, isWatched: false });
+		movieStore.activeTab = 1;
+	};
+
+	return {
+		loader,
+		movies,
+		getMovies,
+		addFavourite,
+	};
 });
